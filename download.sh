c803f6ff192fecf056ebbf880898b593cd21420c
Rotsen Mark Acob <hello@rotsenacob.com>
2024-06-14 08:57:12 +0800

    Updated script

--- download.sh
+++ download.sh
@@ -1,57 +1,52 @@
 #!/bin/bash
-# Create a file if youtube-dl-playlists.txt doesnt exist
-CWD=$(pwd)
 
-if [ ! -f youtube-dl-playlists.txt ]; then
+# Create a file if youtube-dl-playlists.txt does not exist
+if [[ ! -f youtube-dl-playlists.txt ]]; then
   touch youtube-dl-playlists.txt
 fi
 
 # Get input from user
-echo Enter URL
-read videourl
+printf "Enter URL: "
+read -r videourl
 
 # Check if videourl is already in the file
-if grep -R "$videourl" youtube-dl-playlists.txt; then
-  echo "Video already in file"
+if grep -Fxq "$videourl" youtube-dl-playlists.txt; then
+  printf "Video already in file\n"
 else
-  echo "$videourl" >> youtube-dl-playlists.txt
+  printf "%s\n" "$videourl" >> youtube-dl-playlists.txt
 fi
 
 # Execute command
-# yt-dlp --config-location youtube-dl.conf
-
 prompt="Choose an option:"
 formats=("video" "audio" "playlist" "audio-playlist")
 
 PS3="$prompt "
 
-# formats=($formats)
-
 select format in "${formats[@]}"; do
   case $format in
     "video")
-      echo "Downloading video"
-      yt-dlp --config-location $CWD/config/video.conf
-      exit 1
+      printf "Downloading video\n"
+      yt-dlp --config-location "$(dirname "$0")/config/video.conf"
+      exit 0
       ;;
     "audio")
-      echo "Downloading audio"
-      yt-dlp --config-location $CWD/config/audio.conf
-      exit 1
+      printf "Downloading audio\n"
+      yt-dlp --config-location "$(dirname "$0")/config/audio.conf"
+      exit 0
       ;;
     "playlist")
-      echo "Downloading video playlist"
-      yt-dlp --config-location $CWD/config/video-playlist.conf
-      exit 1
+      printf "Downloading video playlist\n"
+      yt-dlp --config-location "$(dirname "$0")/config/video-playlist.conf"
+      exit 0
       ;;
     "audio-playlist")
-      echo "Downloading audio playlist"
-      yt-dlp --config-location $CWD/config/audio-playlist.conf
-      exit 1
+      printf "Downloading audio playlist\n"
+      yt-dlp --config-location "$(dirname "$0")/config/audio-playlist.conf"
+      exit 0
       ;;
     *)
-      echo "Invalid option"
+      printf "Invalid option\n"
       exit 1
-      ;;  
+      ;;
   esac
 done
